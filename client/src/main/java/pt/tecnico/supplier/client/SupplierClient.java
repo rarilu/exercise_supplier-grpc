83c5aa8f3f5bc37fbc82e4ed4995a1e43c992c59
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2020-05-15 18:39:13 +0100

    Add client module

---
+++ client/src/main/java/pt/tecnico/supplier/client/SupplierClient.java
@@ -0,0 +1,76 @@
+package pt.tecnico.supplier.client;
+
+import static javax.xml.bind.DatatypeConverter.printHexBinary;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import pt.tecnico.supplier.grpc.ProductsRequest;
+import pt.tecnico.supplier.grpc.ProductsResponse;
+import pt.tecnico.supplier.grpc.SupplierGrpc;
+
+public class SupplierClient {
+
+	/**
+	 * Set flag to true to print debug messages. The flag can be set using the
+	 * -Ddebug command line option.
+	 */
+	private static final boolean DEBUG_FLAG = (System.getProperty("debug") != null);
+
+	/** Helper method to print debug messages. */
+	private static void debug(String debugMessage) {
+		if (DEBUG_FLAG)
+			System.err.println(debugMessage);
+	}
+
+	public static void main(String[] args) throws Exception {
+		System.out.println(SupplierClient.class.getSimpleName() + " starting ...");
+
+		// Receive and print arguments.
+		System.out.printf("Received %d arguments%n", args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.printf("arg[%d] = %s%n", i, args[i]);
+		}
+
+		// Check arguments.
+		if (args.length < 2) {
+			System.err.println("Argument(s) missing!");
+			System.err.printf("Usage: java %s host port%n", SupplierClient.class.getName());
+			return;
+		}
+
+		final String host = args[0];
+		final int port = Integer.parseInt(args[1]);
+		final String target = host + ":" + port;
+
+		// Channel is the abstraction to connect to a service end-point.
+		final ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+
+		// Create a blocking stub for making synchronous remote calls.
+		SupplierGrpc.SupplierBlockingStub stub = SupplierGrpc.newBlockingStub(channel);
+
+		// Prepare request.
+		ProductsRequest request = ProductsRequest.newBuilder().build();
+		System.out.println("Request to send:");
+		System.out.println(request.toString());
+		debug("in binary hexadecimals:");
+		byte[] requestBinary = request.toByteArray();
+		debug(printHexBinary(requestBinary));
+		debug(String.format("%d bytes%n", requestBinary.length));
+
+		// Make the call using the stub.
+		System.out.println("Remote call...");
+		ProductsResponse response = stub.listProducts(request);
+
+		// Print response.
+		System.out.println("Received response:");
+		System.out.println(response.toString());
+		debug("in binary hexadecimals:");
+		byte[] responseBinary = response.toByteArray();
+		debug(printHexBinary(responseBinary));
+		debug(String.format("%d bytes%n", responseBinary.length));
+
+		// A Channel should be shutdown before stopping the process.
+		channel.shutdownNow();
+	}
+
+}
